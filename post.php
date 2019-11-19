a4288813c43cf7a485418aabe0f4ae2656c5c6a8
Static CDN <57913796+staticfilehost@users.noreply.github.com>
2019-11-19 02:27:33 -0800

    Update post.php

--- post.php
+++ post.php
@@ -1,14 +1,8 @@
-<?php
-// Demonstrate the use of header() function 
-// to refresh the current page 
-   
-echo "Welcome to index page</br>"; 
-echo "Page will refresh in every 3 seconds</br></br>"; 
-    
-// The function will refresh the page  
-// in every 3 second 
-header("refresh: 3"); 
-    
-echo date('H:i:s Y-m-d'); 
-exit; 
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
